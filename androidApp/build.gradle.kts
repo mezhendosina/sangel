b416b072e632d9899ff322e604caa254bc6b5c43
mezhendosina <mensh.zheya@gmail.com>
2024-05-31 17:27:39 +0300

    feat: v0.1.1 release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.sangel.android"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
