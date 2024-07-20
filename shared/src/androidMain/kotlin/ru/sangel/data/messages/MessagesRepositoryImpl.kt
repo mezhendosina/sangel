c405736f3e3f1896cada421efd2b4f315814deb3
mezhendosina <mensh.zheya@gmail.com>
2024-07-20 17:48:16 +0300

    feat: add link to yandex maps in sms message to favorites

--- shared/src/androidMain/kotlin/ru/sangel/data/messages/MessagesRepositoryImpl.kt
+++ shared/src/androidMain/kotlin/ru/sangel/data/messages/MessagesRepositoryImpl.kt
@@ -5,20 +5,24 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.context.GlobalContext.get
 import org.koin.java.KoinJavaComponent.inject
 import ru.sangel.R
+import ru.sangel.app.data.map.MapKitRepository
 import ru.sangel.data.contacts.ContactsRepository
 import ru.sangel.data.settings.AppPrefs
 
-actual class MessagesRepositoryImpl() : MessagesRepository, KoinComponent {
+actual class MessagesRepositoryImpl :
+    MessagesRepository,
+    KoinComponent {
     private val messagesSource: MessagesSource by inject(MessagesSource::class.java)
     private val contactsRepository: ContactsRepository by inject(ContactsRepository::class.java)
     private val appPrefs: AppPrefs by inject(AppPrefs::class.java)
 
     override suspend fun sendMessageToFavorites() {
         val favs = contactsRepository.favorites.first()
-        val context = getKoin().get() as Context
+        val context = get<Context>()
 
         favs.forEach {
             withContext(Dispatchers.IO) {
@@ -36,5 +40,13 @@ actual class MessagesRepositoryImpl() : MessagesRepository, KoinComponent {
     override suspend fun sendMessagesToNearUsers() {
     }
 
-    private fun getLocation(): String = "TODO: get location"
+    private fun getLocation(): String? {
+        val mapKitRepository = get<MapKitRepository>()
+        val location = mapKitRepository.getLocation()
+        return if (location != null) {
+            "https://yandex.ru/maps/?ll=${location.longitude},${location.latitude}&z=13&l=map"
+        } else {
+            null
+        }
+    }
 }
