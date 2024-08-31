1f1c939db293778ae23a15a184dd2b9c57aceb27
mezhendosina <mensh.zheya@gmail.com>
2024-08-31 16:57:08 +0300

    feat: debug

--- shared/src/commonMain/kotlin/ru/sangel/Exceptions.kt
+++ shared/src/commonMain/kotlin/ru/sangel/Exceptions.kt
@@ -4,6 +4,4 @@ import android.content.res.Resources.NotFoundException
 
 class UserNotFoundException : NotFoundException("user_id")
 
-class TokenNotFoundException : NullPointerException()
-
 class TokenNotFoundException() : NullPointerException()
