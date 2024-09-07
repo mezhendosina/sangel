0998674e8f41b2a50960d6e9080b378888f44e76
yakov <yakovrussian@gmail.com>
2024-09-07 16:35:33 +0300

    fix: make sending sms work by fixing map method
    change map method to custom accuracy for reliable work, change compose version to 1.7.0 for LazyColumn on Pixel phones, change toast method to post on Main Thread, get rid of BundledSQLiteDriver because of Huawei error when creating db

--- shared/src/androidMain/kotlin/ru/sangel/data/map/MapRepositoryImpl.kt
+++ shared/src/androidMain/kotlin/ru/sangel/data/map/MapRepositoryImpl.kt
@@ -1,12 +1,13 @@
 package ru.sangel.data.map
 
+import com.yandex.mapkit.location.FilteringMode
 import com.yandex.mapkit.location.Location
 import com.yandex.mapkit.location.LocationListener
+import com.yandex.mapkit.location.LocationManager
 import com.yandex.mapkit.location.LocationStatus
 import com.yandex.mapkit.map.CameraPosition
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -15,6 +16,8 @@ import org.koin.core.component.get
 import ru.sangel.app.data.map.MapRepository
 import ru.sangel.data.map.entities.LocationEntity
 import ru.sangel.data.users.UsersRepository
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MapRepositoryImpl(
     private val usersRepository: UsersRepository,
@@ -52,27 +55,10 @@ class MapRepositoryImpl(
     }
 
     override suspend fun getLinkLocation(): String? {
-        val locationManager = get<com.yandex.mapkit.location.LocationManager>()
-        var location: LocationEntity? = LocationEntity.initValue()
-        locationManager.requestSingleUpdate(
-            object : LocationListener {
-                override fun onLocationUpdated(p0: Location) {
-                    location = LocationEntity(p0.position.latitude, p0.position.longitude)
-                }
+        val locationManager = get<LocationManager>()
+        val location = getLocationCoarse(locationManager)
 
-                override fun onLocationStatusUpdated(p0: LocationStatus) {
-                    if (p0 == LocationStatus.NOT_AVAILABLE) {
-                        location = null
-                    }
-                }
-            },
-        )
-
-        while (location == LocationEntity.initValue()) {
-            delay(1)
-        }
-
-        return if (location != null) "https://yandex.ru/maps/?pt=${location!!.longitude},${location!!.latitude}&l=map" else null
+        return location?.let { "https://yandex.ru/maps/?pt=${it.longitude},${it.latitude}&l=map" }
     }
 
     override suspend fun updateLocation(
@@ -81,4 +67,37 @@ class MapRepositoryImpl(
     ) {
         usersRepository.setMineLocation(latitude, longtitude)
     }
+
+    private suspend fun getLocationCoarse(
+        locationManager: LocationManager
+    ): LocationEntity? = suspendCoroutine { continuation ->
+        val locationListener = object : LocationListener {
+            override fun onLocationUpdated(p0: Location) {
+                val locationEntity = LocationEntity(p0.position.latitude, p0.position.longitude)
+                locationManager.unsubscribe(this)
+                continuation.resume(locationEntity)
+            }
+
+            override fun onLocationStatusUpdated(p0: LocationStatus) {
+                if (p0 == LocationStatus.NOT_AVAILABLE) {
+                    locationManager.unsubscribe(this)
+                    continuation.resume(null)
+                }
+            }
+        }
+        val accuracyMeters = 10.0
+        val minUpdateTimeMillis = 1_000L
+        val minUpdateDistanceMeters = 10.0
+        val allowUseInBackground = false
+        val filteringMode = FilteringMode.ON
+        locationManager.subscribeForLocationUpdates(
+            accuracyMeters,
+            minUpdateTimeMillis,
+            minUpdateDistanceMeters,
+            allowUseInBackground,
+            filteringMode,
+            locationListener
+        )
+    }
+
 }
--- shared/src/commonMain/kotlin/ru/sangel/Exceptions.kt
+++ shared/src/commonMain/kotlin/ru/sangel/Exceptions.kt
@@ -5,3 +5,5 @@ import android.content.res.Resources.NotFoundException
 class UserNotFoundException() : NotFoundException("user_id")
 
 class TokenNotFoundException() : NullPointerException()
+
+class EmergencyNumberNotFountException : NotFoundException("Emergence Number Not Found")
